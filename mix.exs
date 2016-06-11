a6b89d00ad20e5abe36c1dcbfe27d3dc99fd1854
Blake Williams <blake@blakewilliams.me>
2016-06-11 16:22:32 -0400

    Add license and hex publish information

--- mix.exs
+++ mix.exs
@@ -8,6 +8,8 @@ defmodule Doorman.Mixfile do
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: "Tools to make Elixir authentication simple and flexible",
+     package: package,
      deps: deps]
   end
 
@@ -21,4 +23,13 @@ defmodule Doorman.Mixfile do
 
   defp elixirc_paths(:test), do: ["lib", "test/support", "test"]
   defp elixirc_paths(_),     do: ["lib"]
+
+  defp package do
+    [
+      name: :doorman,
+      maintainers: ["Blake Williams", "Ashley Foster"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/BlakeWilliams/doorman"}
+    ]
+  end
 end
