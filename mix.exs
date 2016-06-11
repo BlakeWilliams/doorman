5ef36a255ed1c4e3982566551ae79663b5900dd8
Blake Williams <blake@blakewilliams.me>
2016-06-11 18:50:33 -0400

    Bump version to 0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Doorman.Mixfile do
 
   def project do
     [app: :doorman,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
